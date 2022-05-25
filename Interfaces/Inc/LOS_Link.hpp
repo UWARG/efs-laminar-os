288e32124767091a75d1ef253a1c76def258e348
Anthony Luo <anthonyluo100@Gmail.com>
2022-05-25 10:48:39 -0400

    Actuators & Link interface template

--- Interfaces/Inc/LOS_Link.hpp
+++ Interfaces/Inc/LOS_Link.hpp
@@ -8,7 +8,31 @@
 #ifndef INC_LOS_LINK_HPP_
 #define INC_LOS_LINK_HPP_
 
+#include "../../Drivers/Actuators/Inc/PWM.hpp"
+#include "../../Drivers/Comms/Inc/PPM.hpp"
+#include "../Inc/interface_datatypes.hpp"
 
+class LOS_Link {
+    public:
+        /**
+         * @brief Construct a new los link object
+         * 
+         * @param num_channels 8 if not provided
+         */
+        LOS_Link(uint8_t num_channels = 8);
+
+        
+        /**
+         * @brief Returns a percent value that was most recently received from all channels
+         * Percentage 0-100.
+         * 
+         * @param num 
+         * @return 0 if an invalid channel number was given.
+         */
+        Instructions_t get_inputs();
+    private:
+        PPMChannel *ppm;
+};
 
 
 
--- Interfaces/Src/LOS_Link.cpp
+++ Interfaces/Src/LOS_Link.cpp
@@ -8,3 +8,17 @@
 
 
 
+// #include "../../Drivers/Comms/Inc/PPM.hpp"
+#include "../Inc/LOS_Link.hpp"
+
+LOS_Link::LOS_Link(uint8_t channels) {
+    this->ppm = new PPMChannel(channels);
+    // PPMChannel *ppm = new PPMChannel(channels);
+}
+
+Instructions_t LOS_Link::get_inputs(){
+    Instructions_t instructions;
+    for(uint8_t i=0; i<8; ++i) {
+        instructions.input[i] = this->ppm->get(i);
+    }
+}
