e2b18452e6e61b233252697e12a618954aa0c695
Christopher Chung <tps90067@gmail.com>
2022-10-22 17:54:16 -0500

    comment out main function in boardfiles for stm32f401re

--- boardfiles/nucleof401re/Core/Src/main.c
+++ boardfiles/nucleof401re/Core/Src/main.c
@@ -68,63 +68,63 @@ void MX_FREERTOS_Init(void);
   * @brief  The application entry point.
   * @retval int
   */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
+// int main(void)
+// {
+//   /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+//   /* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+//   /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+//   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+//   HAL_Init();
 
-  /* USER CODE BEGIN Init */
+//   /* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+//   /* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+//   /* Configure the system clock */
+//   SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+//   /* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+//   /* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_USART2_UART_Init();
-  MX_DMA_Init();
-  MX_TIM3_Init();
-  MX_TIM1_Init();
-  MX_ADC1_Init();
-  MX_I2C1_Init();
-  MX_SPI2_Init();
-  MX_SPI3_Init();
-  MX_TIM2_Init();
-  MX_TIM5_Init();
-  MX_USART1_UART_Init();
-  MX_USART6_UART_Init();
-  /* USER CODE BEGIN 2 */
+//   /* Initialize all configured peripherals */
+//   MX_GPIO_Init();
+//   MX_USART2_UART_Init();
+//   MX_DMA_Init();
+//   MX_TIM3_Init();
+//   MX_TIM1_Init();
+//   MX_ADC1_Init();
+//   MX_I2C1_Init();
+//   MX_SPI2_Init();
+//   MX_SPI3_Init();
+//   MX_TIM2_Init();
+//   MX_TIM5_Init();
+//   MX_USART1_UART_Init();
+//   MX_USART6_UART_Init();
+//   /* USER CODE BEGIN 2 */
 
-  /* USER CODE END 2 */
+//   /* USER CODE END 2 */
 
-  /* Call init function for freertos objects (in freertos.c) */
-  MX_FREERTOS_Init();
+//   /* Call init function for freertos objects (in freertos.c) */
+//   MX_FREERTOS_Init();
 
-  /* Start scheduler */
-  osKernelStart();
+//   /* Start scheduler */
+//   osKernelStart();
 
-  /* We should never get here as control is now taken by the scheduler */
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
+//   /* We should never get here as control is now taken by the scheduler */
+//   /* Infinite loop */
+//   /* USER CODE BEGIN WHILE */
+//   while (1)
+//   {
+//     /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
-}
+//     /* USER CODE BEGIN 3 */
+//   }
+//   /* USER CODE END 3 */
+// }
 
 /**
   * @brief System Clock Configuration
