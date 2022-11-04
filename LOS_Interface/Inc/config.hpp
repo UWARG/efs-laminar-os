a22d54391376b548838d8666522d2e5143987383
GuyBlumenthal <guy.blumenthal@outlook.com>
2022-11-03 22:09:02 -0400

    Formatting, style and a few minor tweaks

--- LOS_Interface/Inc/config.hpp
+++ LOS_Interface/Inc/config.hpp
@@ -8,8 +8,8 @@
 
 #include "main.h"
 
-#include "PWMChannel.hpp"
-#include "DSHOTChannel.hpp"
+#include "LOS_D_PWMChannel.hpp"
+#include "LOS_D_DSHOTChannel.hpp"
 
 /* LOS_Link */
 #define PPM
@@ -62,14 +62,13 @@ static Gps *g_gpsObj = NEOM8::GetInstance(); // NEOM8 Gps
 /* LOS_Actuators */
 const uint8_t NUM_CHANNELS = 2;
 
-// DSHOTChannel    channel1(PWM1_Pin, PWM1_GPIO_Port, &htim1, TIM_CHANNEL_1, TIM_DMA_ID_CC1, TIM_DMA_CC1);
-// PWMChannel      channel2(PWM6_Pin, PWM6_GPIO_Port, &htim3, TIM_CHANNEL_1);
+// DSHOTChannel channel1(PWM1_Pin, PWM1_GPIO_Port, &htim1, TIM_CHANNEL_1, TIM_DMA_ID_CC1, TIM_DMA_CC1);
+// PWMChannel   channel2(PWM6_Pin, PWM6_GPIO_Port, &htim3, TIM_CHANNEL_1);
 
-// For test build:
+// For example test build:
 DSHOTChannel    channel1(0, nullptr, nullptr, 0, 0, 0);
 PWMChannel      channel2(1, nullptr, nullptr, 1);
 
-
-static MotorChannel *MOTOR_CHANNELS[NUM_CHANNELS] = {&channel1, &channel2};
+static MotorChannel *motor_channels_[NUM_CHANNELS] = {&channel1, &channel2};
 
 #endif
