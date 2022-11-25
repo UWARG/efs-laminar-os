6170da6c08706fb9daa41a39d7dc5b47b920c108
Guy Blumenthal <guy.blumenthal@outlook.com>
2022-11-25 16:52:48 -0500

    renamed var

--- LOS_Driver/Motor_Channel/Src/LOS_D_PWMChannel.cpp
+++ LOS_Driver/Motor_Channel/Src/LOS_D_PWMChannel.cpp
@@ -22,7 +22,7 @@ void PWMChannel::set(uint8_t percent) {
 
     uint32_t us = (percent * (MAX_SIGNAL - MIN_SIGNAL)) / 100 + MIN_SIGNAL;
     uint32_t ticks = static_cast<uint32_t>((static_cast<float>(us) / static_cast<float>(PWM_PERIOD)) 
-                        * static_cast<float>(period_ticks));
+                        * static_cast<float>(period_ticks_));
 
     __HAL_TIM_SET_COMPARE(timer_, timer_channel_, ticks);
 }
