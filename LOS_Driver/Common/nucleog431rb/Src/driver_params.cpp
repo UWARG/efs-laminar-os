03923813a9f4d76e22f525b0a142f329dd43d3e9
Guy Blumenthal <guy.blumenthal@outlook.com>
2022-11-25 11:16:08 -0500

    Switched to g474

--- LOS_Driver/Common/nucleog431rb/Src/driver_params.cpp
+++
@@ -1,2 +0,0 @@
-#include "driver_params.hpp"
-
---
+++ LOS_Driver/Common/nucleog474re/Src/driver_params.cpp
@@ -0,0 +1,23 @@
+#include "driver_params.hpp"
+#include "config.h"
+
+#include "LOS_D_DSHOTChannel.h"
+#include "LOS_D_PWMChannel.h"
+
+#define PWM1_PIN  0 
+#define PWM1_PORT nullptr
+
+#define PWM2_PIN  0 
+#define PWM2_PORT nullptr
+
+#define TIM1_HANDLE &htim1
+#define TIM2_HANDLE &htim2
+
+
+
+DSHOTChannel dshotChannel = DSHOTChannel(PWM1_PIN, PWM1_PORT, TIM1_HANDLE, 
+                        TIM_CHANNEL_1, TIM_DMA_ID_CC1, 
+                        TIM_DMA_CC1);
+
+PWMChannel pwmChannel = PWMChannel(PWM2_PIN, PWM2_PORT, 
+                    TIM2_HANDLE, TIM_CHANNEL_1);
