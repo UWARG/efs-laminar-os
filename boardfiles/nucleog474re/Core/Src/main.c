f5f5ab381a065991b0fb963e6590af48b4aad8e8
Guy Blumenthal <guy.blumenthal@outlook.com>
2022-11-25 11:47:44 -0500

    added freertos to g4

--- boardfiles/nucleog474re/Core/Src/main.c
+++ boardfiles/nucleog474re/Core/Src/main.c
@@ -68,40 +68,22 @@ void MX_FREERTOS_Init(void);
   */
 int losInit(void)
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_LPUART1_UART_Init();
   MX_TIM1_Init();
   MX_ADC1_Init();
   MX_TIM2_Init();
-  /* USER CODE BEGIN 2 */
 
-  /* USER CODE END 2 */
-
-  /* Init scheduler */
-  osKernelInitialize();  /* Call init function for freertos objects (in freertos.c) */
+  /* Call init function for freertos objects (in freertos.c) */
   MX_FREERTOS_Init();
+
+  osKernelStart();
+
 
 }
 
