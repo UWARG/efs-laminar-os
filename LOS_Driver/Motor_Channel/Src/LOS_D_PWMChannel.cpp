34c0299d08727a78d3859b72cceb7e1f87209fe3
Guy Blumenthal <guy.blumenthal@outlook.com>
2022-11-25 14:45:50 -0500

    Testing changes

--- LOS_Driver/Motor_Channel/Src/LOS_D_PWMChannel.cpp
+++ LOS_Driver/Motor_Channel/Src/LOS_D_PWMChannel.cpp
@@ -6,15 +6,15 @@ PWMChannel::PWMChannel(uint16_t pin_num, GPIO_TypeDef* port, TIM_HandleTypeDef* 
                                                     timer_(timer), 
                                                     timer_channel_(timer_channel) {
     HAL_TIM_PWM_Start(timer, timer_channel);
-
-    uint32_t prescalar = timer->Init.Prescaler;
-    uint32_t period_ticks = timer->Init.Period;
 }
 
 void PWMChannel::set(uint8_t percent) {
     if (percent > 100) {
         return;
     }
+
+    uint32_t prescalar = timer_->Init.Prescaler;
+    uint32_t period_ticks = timer_->Init.Period;
     
     uint32_t us = (percent * (MAX_SIGNAL - MIN_SIGNAL)) / 100 + MIN_SIGNAL;
     uint32_t ticks = static_cast<uint32_t>((static_cast<float>(us) / static_cast<float>(PWM_PERIOD)) 
